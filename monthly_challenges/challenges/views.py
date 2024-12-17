5597587c2b3563aaf4a4ee84de7d7ccb71799bc2
Maciej Kowalski <maciej.ko1444@gmail.com>
2024-12-17 20:24:02 +0100

    dynamic urls

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -21,10 +22,11 @@ monthly_challenges = {
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
-    if month <= 0 or month > 12:
+    if month <= 0 or month > len(months):
         return HttpResponseNotFound("Month not found!")
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
+    return HttpResponseRedirect(redirect_path)
     
 
 def monthly_challenge(request, month):
